99c86cbe784ebe3f218394772d3fc90260578559
He jidong-2 <1941109171@qq.com>
2021-10-22 11:53:33 +0800

    还是有bug,collectionview被覆盖了

--- LittlePink/NoteEdit/NoteEditVC.swift
+++ LittlePink/NoteEdit/NoteEditVC.swift
@@ -6,9 +6,6 @@
 //
 
 import UIKit
-import YPImagePicker
-import SKPhotoBrowser
-import AVKit
 
 class NoteEditVC: UIViewController {
     
@@ -21,131 +18,24 @@ class NoteEditVC: UIViewController {
     
     @IBOutlet weak var photoCollectionview: UICollectionView!
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var titleCountLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     var photoCount:Int{ photos.count }
     var isVideo: Bool{videoURL != nil}
     
     override func viewDidLoad() {
-        photoCollectionview.dragInteractionEnabled = true //开启拖放交互
         super.viewDidLoad()
+        photoCollectionview.dragInteractionEnabled = true //开启拖放交互
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func TFEditBegin(_ sender: Any) {
+        titleCountLabel.isHidden = false
     }
-    */
-
-}
-extension NoteEditVC: UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        photos.count
+    @IBAction func TFEditEnd(_ sender: Any) {
+        titleCountLabel.isHidden = true
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kPhotoCellID, for: indexPath) as! PhotoCell
-        
-        cell.imageView.image = photos[indexPath.item]
-        
-        return cell
-    }
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
-        case UICollectionView.elementKindSectionFooter:
-            let photoFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: kPhotoFooterID, for: indexPath) as! PhotoFooter
-            photoFooter.addPhotoBtn.addTarget(self, action: #selector(addPhoto), for: .touchUpInside)
-            return photoFooter
-        default:
-            fatalError("collectionView的footer出问题")
-        }
-    }
-}
-
-extension NoteEditVC: UICollectionViewDelegate{
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        if isVideo {
-            let playerVC = AVPlayerViewController()
-            playerVC.player = AVPlayer(url: videoURL!)
-            present(playerVC, animated: true){
-                playerVC.player?.play()
-            }
-        } else {
-            // 1. create SKPhoto Array from UIImage
-            var images: [SKPhoto] = []
-            
-            for photo in photos{
-                images.append(SKPhoto.photoWithImage(photo))
-            }
-            // 2. create PhotoBrowser Instance, and present from your viewController.
-            let browser = SKPhotoBrowser(photos: images, initialPageIndex: indexPath.item)
-            browser.delegate = self
-            SKPhotoBrowserOptions.displayAction = false
-            SKPhotoBrowserOptions.displayDeleteButton = true
-            present(browser, animated: true)
-
-        }
-
-    }
-}
-// MARK: - SKPhotoBrowserDelegate
-extension NoteEditVC:SKPhotoBrowserDelegate{
-    func removePhoto(_ browser: SKPhotoBrowser, index: Int, reload: @escaping (() -> Void)) {
-        photos.remove(at: index)
-        photoCollectionview.reloadData()
-        reload()
-    }
-    
-}
-
-//MARK: - 监听
-extension NoteEditVC{
-    @objc private func addPhoto(){
-        if photoCount < kMaxPhotoCounts {
-            var config = YPImagePickerConfiguration()
-            
-            //MARK: -通用配置
-            config.albumName = Bundle.main.appName
-            config.screens = [.library]
-            
-            //照片视频不能混排，多选视频会合成一个视频，编辑页可追加视频或照片，笔记允许发布多张照片或者单个视频
-            
-            //MARK: -相册配置
-            config.library.defaultMultipleSelection = true
-            config.library.maxNumberOfItems = kMaxPhotoCounts - photoCount
-            config.library.spacingBetweenItems = kSpacingBetweenItems
-            
-            //MARK: -视频配置 all default
-            
-            //MARK: -画廊配置(多选后的照片廊)
-            config.gallery.hidesRemoveButton = false
-
-           
-            let picker = YPImagePicker(configuration: config)
-            
-            //MARK: -选完后或按取消按钮后的异步回调处理（依据配置处理单个或多个）
-            picker.didFinishPicking { [unowned picker] items, cancelled in
-                if cancelled {
-                    print("用户按了左上角的取消")
-                }
-               for item in items {
-                if case let .photo(photo) = item {
-                    self.photos.append(photo.image)
-                }
-            }
-                self.photoCollectionview.reloadData()
-
-                picker.dismiss(animated: true)
-            }
-            present(picker, animated: true)
-            
-        } else {
-            
-            self.showTextHUD("最多只能选择\(kMaxPhotoCounts)张照片哦")
-        }
-    }
 }
