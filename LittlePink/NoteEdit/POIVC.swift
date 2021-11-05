6932527acc3e8bec3cbd0e77e0d2048037ab50dd
He jidong-2 <1941109171@qq.com>
2021-11-05 14:31:04 +0800

    none

--- LittlePink/NoteEdit/POIVC.swift
+++ LittlePink/NoteEdit/POIVC.swift
@@ -17,15 +17,15 @@ class POIVC: UIViewController {
 
 }
 
-extension POIVC: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-}
-extension POIVC: UITableViewDelegate{
-    
-}
+//extension POIVC: UITableViewDataSource{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//}
+//extension POIVC: UITableViewDelegate{
+//    
+//}
